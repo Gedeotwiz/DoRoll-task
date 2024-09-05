7d98b5aa52fcc895312569740bccc600091877b6
Gedeotwiz <gedeontwizerimana6@gmail.com>
2024-09-05 10:00:42 +0200

    add refeshing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,16 +4,24 @@ import { ConfigProvider } from "antd";
 import { Provider } from "react-redux";
 import { store } from "@/components/redux/store/store";
 import theme from '../components/util/themeConfig';
+import { QueryClient, QueryClientProvider } from "react-query";
+import { useState } from "react";
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
+   const [queryClient] = useState(() => new QueryClient());
+
   return (
-      
+   <QueryClientProvider client={queryClient}>
+
      <Provider store={store}>
          <ConfigProvider theme={theme}>
              <Component {...pageProps} />
           </ConfigProvider>
      </Provider>
-      
+     </QueryClientProvider>
+
     
   );
 }
